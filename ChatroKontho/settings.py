ba4b7089c4a3ca39553ce977938f4cabf4de8726
Salehin-07 <mdabusalehin123@gmail.com>
2026-04-24 20:45:45 +0600

    changed Debug

--- ChatroKontho/settings.py
+++ ChatroKontho/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY','hdjdlpakajsjsfjsjajwuwhh267992knb@@@#####@##usjsjoowi66289oksnsnsbhshsjsjsiwkks')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com','*.onrender.com','daily-obburthan.onrender.com','daily-obburthan.nav.bd','www.daily-obburthan.nav.bd']
 
